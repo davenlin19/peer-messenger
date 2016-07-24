93b2a26051c4340e41a04106777d731d6a10f171
Trong Lam Phan <lampt2509@gmail.com>
2016-07-24 19:55:27 +0200

    init

--- server/peer-server.js
+++ server/peer-server.js
@@ -1,2 +1,14 @@
+var fs = require('fs');
 var PeerServer = require('peer').PeerServer;
-var server = PeerServer({port: 9000, path: '/peerjs'});
+
+var privateKey  = fs.readFileSync('../sslcert/server.key', 'utf8');
+var certificate = fs.readFileSync('../sslcert/server.crt', 'utf8');
+
+var server = PeerServer({
+  port: 9000,
+  path: '/peerjs',
+  ssl: {
+    key: privateKey,
+    cert: certificate
+  }
+});
